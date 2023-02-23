1161b798788664e0abae20998a291876f76c6f53
Islom-E <iermamatov33@gmail.com>
2023-02-23 07:55:00 +0500

    calculator commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const btnsEl = document.querySelectorAll(".btn");
+const screenEl = document.querySelector(".result-screen");
+const equalBtnEl = document.querySelector(".btn-equal");
+const clearBtnEl = document.querySelector(".btn-clear");
+
+btnsEl.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let btnValue = e.target.dataset.num;
+    screenEl.value += btnValue;
+  });
+});
+
+equalBtnEl.addEventListener("click", (e) => {
+  if (screenEl.value === "" || screenEl.value === undefined) {
+    alert("Somthing went wrong !!!!");
+  } else {
+    let cc = eval(screenEl.value);
+    screenEl.value = cc.toFixed(5);
+  }
+});
+
+clearBtnEl.addEventListener("click", () => {
+  screenEl.value = "";
+});
