4b5374d84cd6f38c11626e04151f96c43e1ab0c9
IslomSFD <islom.developerfd@gmail.com>
2023-09-11 21:46:18 +0500

    commit v

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let btnsEl = document.querySelectorAll(".btn");
 let screenEl = document.querySelector(".result-screen");
 let equalBtnEl = document.querySelector(".btn-equal");
-const clearBtnEl = document.querySelector(".btn-clear");
+let clearBtnEl = document.querySelector(".btn-clear");
 
 btnsEl.forEach((item) => {
   item.addEventListener("click", (e) => {
